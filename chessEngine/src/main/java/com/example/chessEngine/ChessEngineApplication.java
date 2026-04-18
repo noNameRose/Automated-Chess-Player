dac86bc9ab6e4ee3a425934a0e0081296b249d87
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-18 17:29:54 -0500

    set up spring boot for back end

---
+++ chessEngine/src/main/java/com/example/chessEngine/ChessEngineApplication.java
@@ -0,0 +1,14 @@
+package com.example.chessEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessEngineApplication {
+
+	public static void main(String[] args) {
+
+    SpringApplication.run(ChessEngineApplication.class, args);
+	}
+
+}
