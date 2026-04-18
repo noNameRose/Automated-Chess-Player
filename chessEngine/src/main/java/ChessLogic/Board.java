989527beca57ba0ef0d426b96957e6ed45298950
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-18 18:06:32 -0500

    set up board

--- chessEngine/src/main/java/ChessLogic/Board.java
+++ chessEngine/src/main/java/ChessLogic/Board.java
@@ -1,4 +1,173 @@
 package ChessLogic;
 
 public class Board {
+  // Instance variables (add more if you need)
+  private Piece[][] board;
+
+
+  /**
+   * Default Constructor
+   */
+  public Board() {
+    // initialize the board to chessboard dimensions.
+  }
+
+  // Accessor Methods
+
+  /**
+   * Gets the piece at a particular row and column of the board.
+   * @param row       The row of the piece to be accessed.
+   * @param col       The column of the piece to be accessed.
+   * @return          The piece at the specified row and column of the board.
+   */
+  public Piece getPiece(int row, int col) {
+  }
+
+  /**
+   * Sets the piece at a particular row and column of the board.
+   * @param row       The row to place the piece at.
+   * @param col       The column to place the piece at.
+   * @param piece     The piece to place at the specified row and column.
+   */
+  public void setPiece(int row, int col, Piece piece) {
+  }
+
+  // Movement helper functions
+
+  /**
+   * Verifies that the source and destination of a move are valid by performing the following checks:
+   *  1. ALL rows and columns provided must be >= 0.
+   *  2. ALL rows and columns provided must be < 8.
+   *  3. The start position of the move must contain a piece.
+   *  4. The piece at the starting position must be the correct color.
+   *  5. The destination must be empty OR must contain a piece of the opposite color.
+   * @param startRow  The starting row of the move.
+   * @param startCol  The starting column of the move.
+   * @param endRow    The ending row of the move.
+   * @param endCol    The ending column of the move.
+   * @param isBlack   The expected color of the starting piece.
+   * @return True if the above conditions are met, false otherwise.
+   */
+  public boolean verifySourceAndDestination(int startRow, int startCol, int endRow, int endCol, boolean isBlack) {
+  }
+
+  /**
+   * Verifies that the source and destination of a move are adjacent squares (within 1 square of each other)
+   * Example, Piece P is adjacent to the spots marked X:
+   * OOOOO
+   * OXXXO
+   * OXPXO
+   * OXXXO
+   * OOOOO
+   * @param startRow  The starting row of the move.
+   * @param startCol  The starting column of the move.
+   * @param endRow    The ending row of the move.
+   * @param endCol    The ending column of the move.
+   * @return True if the source and destination squares are adjacent, false otherwise.
+   */
+  public boolean verifyAdjacent(int startRow, int startCol, int endRow, int endCol) {
+  }
+
+  /**
+   * Verifies that a source and destination are in the same row and that there are no pieces on squares
+   * between the source and the destination.
+   * @param startRow  The starting row of the move.
+   * @param startCol  The starting column of the move.
+   * @param endRow    The ending row of the move.
+   * @param endCol    The ending column of the move.
+   * @return True if source and destination are in same row with no pieces between them, false otherwise.
+   */
+  public boolean verifyHorizontal(int startRow, int startCol, int endRow, int endCol) {
+  }
+
+  /**
+   * Verifies that a source and destination are in the same column and that there are no pieces on squares
+   * between the source and the destination.
+   * @param startRow  The starting row of the move.
+   * @param startCol  The starting column of the move.
+   * @param endRow    The ending row of the move.
+   * @param endCol    The ending column of the move.
+   * @return True if source and destination are in same column with no pieces between them, false otherwise.
+   */
+  public boolean verifyVertical(int startRow, int startCol, int endRow, int endCol) {
+  }
+
+  /**
+   * Verifies that a source and destination are on the same diagonal and that there are no pieces on squares
+   * between the source and the destination.
+   * @param startRow  The starting row of the move.
+   * @param startCol  The starting column of the move.
+   * @param endRow    The ending row of the move.
+   * @param endCol    The ending column of the move.
+   * @return True if source and destination are on the same diagonal with no pieces between them, false otherwise.
+   */
+  public boolean verifyDiagonal(int startRow, int startCol, int endRow, int endCol) {
+  }
+
+  // Game functionality methods
+
+  /**
+   * Moves the piece from startRow, startCol to endRow, endCol if it is legal to do so.
+   * IMPORTANT: Make sure to update the internal position of the piece, and the starting position of the piece to null!
+   * @param startRow  The starting row of the move.
+   * @param startCol  The starting column of the move.
+   * @param endRow    The ending row of the move.
+   * @param endCol    The ending column of the move.
+   * @return Whether the move was successfully completed or not. (Moves are not completed if they are not legal.)
+   */
+  public boolean movePiece(int startRow, int startCol, int endRow, int endCol) {
+
+  }
+
+  /**
+   * Returns true if there are fewer than TWO kings on the board.
+   * @return If the game is in a game over state.
+   */
+  public boolean isGameOver() {
+
+  }
+
+  /**
+   * Sets all indexes in the board to null
+   */
+  public void clear() {
+  }
+
+  public void display() {
+    System.out.print("\t\t\t");
+    for (int i = 0; i < 8; i++) {
+      System.out.print(i + "\t\t");
+    }
+    System.out.print("\n");
+    for (int i = 0; i < 8; i++) {
+      System.out.print("\t" + i + "\t");
+      for (int j = 0; j < 8; j++) {
+        if (board[i][j] == null) {
+          System.out.print("|\t\t");
+        } else {
+          System.out.print("|\t" + board[i][j] + "\t");
+        }
+      }
+      System.out.print("|\n");
+    }
+  }
+
+  public String toString() {
+    StringBuilder out = new StringBuilder();
+    out.append(" ");
+    for(int i = 0; i < 8; i++){
+      out.append(" ");
+      out.append(i);
+    }
+    out.append('\n');
+    for(int i = 0; i < board.length; i++) {
+      out.append(i);
+      out.append("|");
+      for(int j = 0; j < board[0].length; j++) {
+        out.append(board[i][j] == null ? "\u2001|" : board[i][j] + "|");
+      }
+      out.append("\n");
+    }
+    return out.toString();
+  }
 }
