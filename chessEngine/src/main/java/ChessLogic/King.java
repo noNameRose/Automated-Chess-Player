b001dfa79102e63d9d73594ee8040f2bf6007216
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-18 19:48:43 -0500

    finish logic for king

--- chessEngine/src/main/java/ChessLogic/King.java
+++ chessEngine/src/main/java/ChessLogic/King.java
@@ -1,4 +1,17 @@
 package ChessLogic;
 
-public class King {
+public class King extends Piece{
+
+  public King(int row, int col, boolean isBlack) {
+    super.row = row;
+    super.col = col;
+    super.isBlack = isBlack;
+    super.representation = isBlack ? PieceRepresentation.BLACK_KING : PieceRepresentation.WHITE_KING;
+  }
+
+
+  @Override
+  public boolean isMoveLegal(Board board, int endRow, int endCol) {
+    return board.verifyAdjacent(this.row, this.col, endRow, endCol);
+  }
 }
--- chessEngine/src/main/java/ChessLogic/Rook.java
+++ chessEngine/src/main/java/ChessLogic/Rook.java
@@ -1,4 +1,17 @@
 package ChessLogic;
 
-public class Rook {
+public class Rook extends Piece{
+  public Rook(int row, int col, boolean isBlack) {
+    super.row = row;
+    super.col = col;
+    super.isBlack = isBlack;
+    super.representation = isBlack ? PieceRepresentation.BLACK_ROOK : PieceRepresentation.WHITE_ROOK;
+  }
+
+
+  @Override
+  public boolean isMoveLegal(Board board, int endRow, int endCol) {
+    return board.verifyHorizontal(this.row, this.col, endRow, endCol) ||
+           board.verifyVertical(this.row, this.col, endRow, endCol);
+  }
 }
