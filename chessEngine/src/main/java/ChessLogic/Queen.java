12b8baa8f90243d64b66058175f199ff3d96f4fb
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-18 19:36:30 -0500

    finish logic for queen

--- chessEngine/src/main/java/ChessLogic/Queen.java
+++ chessEngine/src/main/java/ChessLogic/Queen.java
@@ -1,4 +1,19 @@
 package ChessLogic;
 
-public class Queen {
+public class Queen extends Piece {
+
+  public Queen(int row, int col, boolean isBlack) {
+    super.row = row;
+    super.col = col;
+    super.isBlack = isBlack;
+    super.representation = isBlack ? PieceRepresentation.BLACK_QUEEN : PieceRepresentation.WHITE_QUEEN;
+  }
+
+
+  @Override
+  public boolean isMoveLegal(Board board, int endRow, int endCol) {
+    return board.verifyDiagonal(this.row, this.col, endRow, endCol) ||
+          board.verifyVertical(this.row, this.col, endRow, endCol) ||
+          board.verifyHorizontal(this.row, this.col, endRow, endCol);
+  }
 }
