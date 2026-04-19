5fb50a4b3dd706fe4aff143911ba7df1ef6ee262
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-18 19:55:18 -0500

    finish logic for knight

--- chessEngine/src/main/java/ChessLogic/Knight.java
+++ chessEngine/src/main/java/ChessLogic/Knight.java
@@ -1,4 +1,20 @@
 package ChessLogic;
 
-public class Knight {
+public class Knight extends Piece{
+  public Knight(int row, int col, boolean isBlack) {
+    super.row = row;
+    super.col = col;
+    super.isBlack = isBlack;
+    super.representation = isBlack ? PieceRepresentation.BLACK_KNIGHT : PieceRepresentation.WHITE_KNIGHT;
+  }
+
+
+  @Override
+  public boolean isMoveLegal(Board board, int endRow, int endCol) {
+    int dy = Math.abs(endRow - this.row);
+    int dx = Math.abs(endCol - this.col);
+    return (dy == 2 && dx == 1) || (dx == 2 && dy == 1);
+  }
+
+
 }
