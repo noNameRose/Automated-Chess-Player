9d7e72a23fc738aedcbda39d0705973570714b62
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-19 22:57:19 -0500

    set up human agent

--- chessEngine/src/main/java/com/example/chessEngine/ChessEngineApplication.java
+++ chessEngine/src/main/java/com/example/chessEngine/ChessEngineApplication.java
@@ -1,6 +1,7 @@
 package com.example.chessEngine;
 
 import ChessLogic.Board;
+import ChessLogic.BoardInitializer;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,10 @@ public class ChessEngineApplication {
 
 	public static void main(String[] args) {
     SpringApplication.run(ChessEngineApplication.class, args);
+
+    Board board = new Board();
+    BoardInitializer.initialize(board);
+    board.display();
 	}
 
 }
