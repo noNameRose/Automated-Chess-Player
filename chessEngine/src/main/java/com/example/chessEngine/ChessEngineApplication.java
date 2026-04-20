58935292aa6b44fb1bec6fb8aa4e6848eaf677bf
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-19 23:06:25 -0500

    add testing for the game

--- chessEngine/src/main/java/com/example/chessEngine/ChessEngineApplication.java
+++ chessEngine/src/main/java/com/example/chessEngine/ChessEngineApplication.java
@@ -1,6 +1,8 @@
 package com.example.chessEngine;
 
 import Agent.Agent;
+import Agent.HumanAgent;
+import Agent.RandomAgent;
 import ChessLogic.Board;
 import ChessLogic.BoardInitializer;
 import jakarta.annotation.PostConstruct;
@@ -18,10 +20,27 @@ public class ChessEngineApplication {
     BoardInitializer.initialize(board);
     board.display();
 
-    Agent humanPlayer = new Agent.HumanAgent()
-
+    Agent player1 = new HumanAgent("Human Agent", true);
+    Agent player2 = new RandomAgent("Random Agent", false);
     while (true) {
 
+      // Player 1 makes move
+      int[] move  = player1.makeMove(board);
+      board.movePiece(move[0], move[1], move[2], move[3]);
+      board.display();
+      if (board.isGameOver()) {
+        System.out.println(player1.getName() + " has won the game");
+        break;
+      }
+
+
+      // Player 2 makes move
+      move = player2.makeMove(board);
+      board.movePiece(move[0], move[1], move[2], move[3]);
+      if (board.isGameOver()) {
+        System.out.println(player2.getName() + " has won the game");
+        break;
+      }
     }
 	}
 
