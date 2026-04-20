0285770077bd8a3e6f3d8558aacbebf0b4f27e39
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-19 21:48:15 -0500

    add shortcut name for each piece

--- chessEngine/src/main/java/ChessLogic/PieceRepresentation.java
+++ chessEngine/src/main/java/ChessLogic/PieceRepresentation.java
@@ -2,7 +2,6 @@ package ChessLogic;
 
 public final class PieceRepresentation {
 
-
   public static final char WHITE_PAWN_CODE = '\u2659';
   public static final char WHITE_KNIGHT_CODE = '\u2658';
   public static final char WHITE_BISHOP_CODE = '\u2657';
@@ -16,6 +15,25 @@ public final class PieceRepresentation {
   public static final char BLACK_ROOK_CODE = '\u265C';
   public static final char BLACK_QUEEN_CODE = '\u265B';
   public static final char BLACK_KING_CODE = '\u265A';
+
+
+  public static final String WHITE_PAWN = "wp";
+  public static final String WHITE_KNIGHT = "wkn";
+  public static final String WHITE_BISHOP = "wb";
+  public static final String WHITE_ROOK = "wr";
+  public static final String WHITE_QUEEN = "wq";
+  public static final String WHITE_KING = "wk";
+
+
+  public static final String BLACK_PAWN = "bp";
+  public static final String BLACK_KNIGHT = "bkn";
+  public static final String BLACK_BISHOP = "bb";
+  public static final String BLACK_ROOK = "br";
+  public static final String BLACK_QUEEN = "bq";
+  public static final String BLACK_KING = "bk";
+
+
+
 
 
 
