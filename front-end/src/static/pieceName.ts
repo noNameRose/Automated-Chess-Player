0a340ab11854f000081e82720ca09f2568d565e7
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 18:17:22 -0500

    set up piece name

---
+++ front-end/src/static/pieceName.ts
@@ -0,0 +1,16 @@
+export const WHITE_PAWN: string = "wp";
+export const WHITE_ROOK: string = "wr";
+export const WHITE_KNIGHT: string = "wkn";
+export const WHITE_BISHOP: string = "wb";
+export const WHITE_QUEEN: string = "wq";
+export const WHITE_KING: string = "wk";
+
+export const BLACK_PAWN: string = "bp";
+export const BLACK_ROOK: string = "br";
+export const BLACK_KNIGHT: string = "bkn";
+export const BLACK_BISHOP: string = "bb";
+export const BLACK_QUEEN: string = "bq";
+export const BLACK_KING: string = "bk";
+
+
+
