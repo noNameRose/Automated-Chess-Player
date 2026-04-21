ec6266718f96c10e8cdc438f5e974c4a7c8b3478
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 17:59:52 -0500

    make font wrapper has width

--- front-end/src/Component/FloatingText.tsx
+++ front-end/src/Component/FloatingText.tsx
@@ -1,20 +1,31 @@
 
-const NUM_SHADOW: number = 15;
+const NUM_SHADOW: number = 20;
 const DX: number = 0.015;
 const DY: number = 0.015;
 
 const FloatingText = ({message}: {message: string}) => {
     const texts: string[] = new Array(NUM_SHADOW).fill(message);
     return (
-        <div className="relative font-bold mx-auto w-fit">
+        <div className="absolute font-bold left-1/2 -translate-x-1/2">
+            <h1
+                className="text-green-200 relative z-100"
+                style={
+                    {
+                        fontSize: "clamp(2.5rem, 7vw, 10rem)",
+                    }
+                }
+            >
+                {message}
+            </h1>
             {texts.map((text: string, index: number) => (
                 <h1 
-                    className={"absolute whitespace-nowrap -translate-x-1/2 translate-y-full " + (index === NUM_SHADOW - 1 ? "text-green-200" : "text-blue-500")}
-                    style={
+                    className={"absolute whitespace-nowrap text-blue-500"}
+                    style={ 
                         {
-                            fontSize: "clamp(2.5rem, 5vw, 10rem)",
-                            top: - (index * DY) + "em",
-                            left: - (index * DX) + "em"
+                            fontSize: "clamp(2.5rem, 7vw, 10rem)",
+                            top: (index * DY) + "em",
+                            left: (index * DX) + "em",
+                            zIndex: - index
                         }
                     }
                 >
