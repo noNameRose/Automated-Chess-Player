f052f529f6c5b181c3e4c499bb3300aa73eba0dd
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 16:49:03 -0500

    set up hero section

---
+++ front-end/src/Component/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <svg viewBox="0 0 300 300" 
+             className="w-80 h-50 border-2"
+        >
+            
+        </svg>
+    );
+};
--- front-end/src/Page/LandingPage.tsx
+++ front-end/src/Page/LandingPage.tsx
@@ -3,7 +3,7 @@ import FloatingText from "../Component/FloatingText";
 const LandingPage = () => {
 
     return (
-        <div className="w-screen min-h-screen bg-gray-50 overflow-hidden">
+        <div className="w-screen min-h-screen bg-gray-50 overflow-hidden mx-auto">
             <FloatingText message="Chess Engine"/>
         </div>
     );
