8ae8b706b17f8cf2d9f00d832cdb7eb559374953
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 20:33:59 -0500

    move text up a little bit

--- front-end/src/Component/FloatingText.tsx
+++ front-end/src/Component/FloatingText.tsx
@@ -1,36 +1,36 @@
 
-const NUM_SHADOW: number = 20;
+const NUM_SHADOW: number = 15;
 const DX: number = 0.015;
 const DY: number = 0.015;
 
 const FloatingText = ({message}: {message: string}) => {
     const texts: string[] = new Array(NUM_SHADOW).fill(message);
     return (
-        <div className="absolute font-bold left-1/2 -translate-x-1/2">
+        <div className="font-bold -translate-y-[5vh]">
             <h1
-                className="text-green-200 relative z-100"
+                className="relative z-100 whitespace-nowrap"
                 style={
                     {
                         fontSize: "clamp(2.5rem, 7vw, 10rem)",
                     }
                 }
             >
-                {message}
-            </h1>
-            {texts.map((text: string, index: number) => (
-                <h1 
-                    className={"absolute whitespace-nowrap text-blue-500"}
-                    style={ 
-                        {
-                            fontSize: "clamp(2.5rem, 7vw, 10rem)",
-                            top: (index * DY) + "em",
-                            left: (index * DX) + "em",
-                            zIndex: - index
+                <span className="text-green-200 relative z-90">{message}</span>
+                {texts.map((text: string, index: number) => (
+                    <span
+                        className={"absolute text-blue-500"}
+                        style={ 
+                            {
+                                top: (index * DY) + "em",
+                                left: (index * DX) + "em",
+                                zIndex: - index
+                            }
                         }
-                    }
-                >
-                    {text}
-                </h1>))}
+                    >
+                        {text}
+                    </span>))
+                }
+            </h1>
         </div>
     );
 };
