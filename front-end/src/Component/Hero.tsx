c60aa61eff339c428646aafce00420f238ebc80f
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 19:19:44 -0500

    update Hero component with PieceList prop

--- front-end/src/Component/Hero.tsx
+++ front-end/src/Component/Hero.tsx
@@ -1,9 +1,18 @@
-const Hero = () => {
+import type { Piece } from "../Entity/Piece";
+import PieceIcon from "./PieceIcon";
+
+const Hero = ({pieceList} : {pieceList: Piece[]}) => {
     return (
-        <svg viewBox="0 0 300 300" 
-             className="w-80 h-50 border-2"
-        >
-            
-        </svg>
+        <div className="relative z-10 w-[80vw] h-[50vh]">
+            <svg viewBox="0 0 300 300" 
+                className="w-full h-full border-2"
+            >
+                {pieceList.map((piece: Piece) => {
+                    return <PieceIcon piece={piece}/>
+                })}
+            </svg>
+        </div>
     );
 };
+
+export default Hero;
