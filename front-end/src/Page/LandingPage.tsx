3fa9e715e6fb4313582ff0eaa8c1a13ddbb4cf68
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 21:17:45 -0500

    test intro animation for piece in landing page

--- front-end/src/Page/LandingPage.tsx
+++ front-end/src/Page/LandingPage.tsx
@@ -1,10 +1,59 @@
+import { useEffect, useRef } from "react";
 import FloatingText from "../Component/FloatingText";
+import Hero from "../Component/Hero";
+import { Piece } from "../Entity/Piece";
+import { BISHOP, KNIGHT, PAWN, QUEEN, ROOK } from "../static/pieceName";
+import gsap from "gsap";
 
 const LandingPage = () => {
+    const tl = useRef<gsap.core.Timeline | null>(null);
+    const blackKnight = new Piece(KNIGHT, 0, 300, true);
+    const blackPawn = new Piece(PAWN, 500, 0, true);
+    const blackBishop = new Piece(BISHOP, 1000, 300, true);
+    const blackRook = new Piece(ROOK, 1500, 100, true);
+    const blackQueen = new Piece(QUEEN, 2200, 300, true);
 
+    const pieceArray = [blackKnight, 
+                        blackPawn, 
+                        blackBishop, 
+                        blackRook, 
+                        blackQueen
+                    ];
+
+    useEffect(() => {
+        tl.current = gsap.timeline();
+
+        tl.current.from([blackKnight.container, 
+                        blackPawn.container,
+                        blackBishop.container,
+                        blackRook.container,
+                        blackQueen.container
+                    ], {
+            y: 1000,
+            delay: .5,
+            stagger: 0.2
+        })
+
+        return () => {
+            if (tl.current) {
+                tl.current.revert();
+                tl.current = null;
+            }
+        }
+    }, []);
     return (
-        <div className="w-screen min-h-screen bg-gray-50 overflow-hidden mx-auto">
+        <div className="w-screen 
+                        min-h-screen 
+                        bg-gray-50 
+                        overflow-hidden 
+                        flex
+                        flex-col
+                        items-center
+                        justify-center
+                        gap-[5em]
+                        ">
             <FloatingText message="Chess Engine"/>
+            <Hero pieceList={pieceArray}/>
         </div>
     );
 };
