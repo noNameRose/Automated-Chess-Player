cf6d8db27c646d3d4808a40e3c81cf3cdc905e40
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 20:15:09 -0500

    remove border and move camera to the left

--- front-end/src/Component/Hero.tsx
+++ front-end/src/Component/Hero.tsx
@@ -3,9 +3,9 @@ import PieceIcon from "./PieceIcon";
 
 const Hero = ({pieceList} : {pieceList: Piece[]}) => {
     return (
-        <div className="relative z-10 w-[80vw] h-[50vh]">
-            <svg viewBox="0 0 1000 1000" 
-                className="w-full h-full border-2"
+        <div className="relative z-10 w-[80vw] h-[40vh]">
+            <svg viewBox="-500 0 1000 1000" 
+                className="w-full h-full"
                 preserveAspectRatio="xMinYMin meet"
             >
                 {pieceList.map((piece: Piece) => {
