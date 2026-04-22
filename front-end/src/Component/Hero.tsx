03570f9e841c6061cb486f1758a2a25b67dca53a
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-04-21 19:56:11 -0500

    add prespective ratio so that the origin is top left corner

--- front-end/src/Component/Hero.tsx
+++ front-end/src/Component/Hero.tsx
@@ -4,8 +4,9 @@ import PieceIcon from "./PieceIcon";
 const Hero = ({pieceList} : {pieceList: Piece[]}) => {
     return (
         <div className="relative z-10 w-[80vw] h-[50vh]">
-            <svg viewBox="0 0 300 300" 
+            <svg viewBox="0 0 1000 1000" 
                 className="w-full h-full border-2"
+                preserveAspectRatio="xMinYMin meet"
             >
                 {pieceList.map((piece: Piece) => {
                     return <PieceIcon piece={piece}/>
