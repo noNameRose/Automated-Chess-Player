bc01c45b3a5429de2d471862d466b72f4fd21eca
noNameRose <datnguyengithubwork@gmail.com>
2026-06-22 08:30:47 +0700

    delete the whole front-end to rebuild everything

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,22 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import LandingPage from "./Page/LandingPage";
-import PixelAnimator from "./Component/PixelAnimator";
-import { PixelController } from "./Entity/PixelController";
-import { useState } from "react";
-import { TransitionContext } from "./context/TransitionContext";
+
 
 
 const App = () => {
-  const pixelController = new PixelController();
-  const [isTransition, setIsTransition] = useState<boolean>(true);
+
   return (
     <>
-      <TransitionContext value={{isTransition: isTransition, setIsTransition: setIsTransition}}>
-        <PixelAnimator pixelController={pixelController}/>
-        <Routes>
-          <Route path="/" element={<LandingPage/>}/>
-        </Routes>
-      </TransitionContext>
+     
     </>
   );
 };
