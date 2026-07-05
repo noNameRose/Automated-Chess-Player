3a014c51a109beb8e635000fa49d7c90915623c1
noNameRose <datnguyengithubwork@gmail.com>
2026-07-05 07:51:06 +0700

    fea: add mouse out handler signature to mouse event context type

--- front-end/src/contexts/MouseEventContext.tsx
+++ front-end/src/contexts/MouseEventContext.tsx
@@ -4,7 +4,9 @@ import type { Player } from "../../public/static/options";
 
 type Handlers = {
     hoverHandler1: (name: Player) => void,
-    hoverHandler2: (name: Player) => void
+    hoverHandler2: (name: Player) => void,
+    mouseOutHandler1: () => void;
+    mouseOutHandler2: () => void;
 }
 
 const MouseEventContext = createContext<Handlers | null>(null);
