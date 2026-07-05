8383abb4ff14f91e7c690b8e0211a540ef7b7255
noNameRose <datnguyengithubwork@gmail.com>
2026-07-05 07:28:10 +0700

    fix: change variable name from agent -> player

--- front-end/public/static/options.tsx
+++ front-end/public/static/options.tsx
@@ -1,7 +1,7 @@
-export type Agent = "Claude" | "ChatGPT" | "Human" | "Random";
+export type Player = "Claude" | "ChatGPT" | "Human" | "Random";
 
 export type option = {
-    name: Agent,
+    name: Player,
     desc: string,
 };
 
@@ -10,18 +10,18 @@ const options: option[] = [
         name: "Claude",
         desc: "This agent make a move based on claude API"
     },
-    // {
-    //     name: "ChatGPT",
-    //     desc: "This agent make a move based on ChatGPT API"
-    // },
-    // {
-    //     name: "Random",
-    //     desc: "This agent choose a random move based on all possible move it has"
-    // },
-    // {
-    //     name: "Human",
-    //     desc: "You are the one who decide what move to take"
-    // }
+    {
+        name: "ChatGPT",
+        desc: "This agent make a move based on ChatGPT API"
+    },
+    {
+        name: "Random",
+        desc: "This agent choose a random move based on all possible move it has"
+    },
+    {
+        name: "Human",
+        desc: "You are the one who decide what move to take"
+    }
 ];
 
 export default options;
