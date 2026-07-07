b6cc297f82977479d3a47096f50926eb7ffbf805
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-07 17:01:08 -0500

    fea: add child prop to pixel transition component

--- front-end/src/pages/PixelTransition.tsx
+++ front-end/src/pages/PixelTransition.tsx
@@ -1,9 +1,9 @@
 import gsap from "gsap";
-import { useEffect, useState } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 
-const PixelTransition = () => {
+const PixelTransition = ({children}: {children: ReactNode}) => {
     const [isShow, setShow] = useState<Boolean>(true);
-    
+
     useEffect(() => {
         if (isShow) {
             gsap.to(document.querySelectorAll("#pixel"), {
@@ -17,23 +17,26 @@ const PixelTransition = () => {
         }
     }, [isShow]);
     return (
-        <div 
-            className="w-screen min-h-screen fixed z-100 grid"
-            style={
-                {
-                    gridTemplateColumns: "repeat(auto-fit, clamp(25px, 4vw, 60px))",
-                    gridTemplateRows: "repeat(auto-fit, clamp(25px, 4vw, 60px))",
+        <>
+            <div 
+                className="w-screen min-h-screen fixed z-100 grid"
+                style={
+                    {
+                        gridTemplateColumns: "repeat(auto-fit, clamp(25px, 4vw, 60px))",
+                        gridTemplateRows: "repeat(auto-fit, clamp(25px, 4vw, 60px))",
+                    }
                 }
-            }
-        >
-            {(new Array(500)).fill(null).map(_ => (
-                <div 
-                    id="pixel"
-                    className="bg-black scale-0"
-                >
-                </div>
-            ))}
-        </div>
+            >
+                {(new Array(500)).fill(null).map(_ => (
+                    <div 
+                        id="pixel"
+                        className="bg-black scale-0"
+                    >
+                    </div>
+                ))}
+            </div>
+            
+        </>
     );
 };
 
