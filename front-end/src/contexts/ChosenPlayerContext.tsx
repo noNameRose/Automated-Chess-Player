8fd88d52d99cacdd5b147cf04384097ab2da73f8
noNameRose <datnguyengithubwork@gmail.com>
2026-07-07 08:30:35 +0700

    fix: make player context holds 2 chosen player instead of one

--- front-end/src/contexts/ChosenPlayerContext.tsx
+++ front-end/src/contexts/ChosenPlayerContext.tsx
@@ -1,6 +1,12 @@
 import { createContext } from "react";
 import type { Player } from "../../public/static/options";
 
-const ChosenPlayerContext = createContext<Player | null>(null);
+type ChosenPlayer = {
+    firstChosenPlayer: Player,
+    secondChosenPlayer: Player
+}
+
+
+const ChosenPlayerContext = createContext<ChosenPlayer | null>(null);
 
 export default ChosenPlayerContext;
