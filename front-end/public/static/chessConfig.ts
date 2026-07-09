bcaf6fb796fec3941b62aa170fcae019fc2905d3
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-09 18:35:59 -0500

    style: add color type for background of each cell

--- front-end/public/static/chessConfig.ts
+++ front-end/public/static/chessConfig.ts
@@ -2,3 +2,11 @@ export type PieceString = "BP" | "BR" | "BQ" | "BKI" | "BK" | "BB"
                   | "WP" | "WR" | "WQ" | "WKI" | "WK" | "WB";
 
 export const CELL_DIMENSION = 100;
+
+type CHATGPT_CELL_FILL = "#4FBDA1";
+type CLAUDE_CELL_FILL = "#4FBDA1";
+type RANDOM_CELL_FILL = "#4B73BD";
+
+
+
+export type CellColor = CHATGPT_CELL_FILL | CLAUDE_CELL_FILL | RANDOM_CELL_FILL;
