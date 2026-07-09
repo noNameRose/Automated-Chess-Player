0dffda6bcd64a57c68e46e9c99b8d1b5af13023e
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-09 15:47:55 -0500

    fea: set up board state

--- front-end/src/chess/Board.tsx
+++ front-end/src/chess/Board.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
+
+type PieceString = "BP" | "BR" | "BQ" | "BKI" | "BK" | "BB" 
+                  | "WP" | "WR" | "WQ" | "WKI" | "WK" | "WB";
+type Piece = PieceString | null;
+
+const initialState: Piece[][]= [
+    ["BR",  "BK",  "BB",  "BQ",  "BKI", "BB",  "BK",  "BR"],
+    ["BP",  "BP",  "BP",  "BP",  "BP",  "BP",  "BP",  "BP"],
+    [null,  null,  null,  null,  null,  null,  null,  null],
+    [null,  null,  null,  null,  null,  null,  null,  null],
+    [null,  null,  null,  null,  null,  null,  null,  null],
+    [null,  null,  null,  null,  null,  null,  null,  null],
+    ["WP",  "WP",  "WP",  "WP",  "WP",  "WP",  "WP",  "WP"],
+    ["WR",  "WK",  "WB",  "WQ",  "WKI", "WB",  "WK",  "WR"]
+];
+
 const Board = () => {
+    const [board, setBoard] = useState<Piece[][] | null>(initialState);
     return (
         <svg viewBox="0 0 500 500" 
-            className="border-2 max-w-[50vw] h-screen"
+            className="border-2 w-screen h-screen"
         >
             <rect
                 width={50}
