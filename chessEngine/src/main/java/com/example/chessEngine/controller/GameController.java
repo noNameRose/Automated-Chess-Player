893ccd9c48567b3bb4369eaff703a41a8518427d
noNameRose <datnguyengithubwork@gmail.com>
2026-07-12 06:38:16 +0700

    fea: add cors config to allow front-end request

--- chessEngine/src/main/java/com/example/chessEngine/controller/GameController.java
+++ chessEngine/src/main/java/com/example/chessEngine/controller/GameController.java
@@ -1,17 +1,20 @@
 package com.example.chessEngine.controller;
 
 
+import ChessLogic.BoardInitializer;
+import com.example.chessEngine.dto.StateResponse;
 import com.example.chessEngine.services.BoardStateService;
-import com.example.chessEngine.services.BoardStateServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@CrossOrigin("http://localhost:5173/*")
 public class GameController {
 
     private final BoardStateService boardStateService;
@@ -21,11 +24,22 @@ public class GameController {
         this.boardStateService = boardStateService;
     }
 
+
+    @GetMapping("/game")
+    public ResponseEntity<StateResponse> startGame() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(StateResponse
+                        .builder()
+                        .state(BoardInitializer.congig).
+                        build());
+    }
+
     @GetMapping("/game/{id}")
     public ResponseEntity<String[][]> getBoardState(@PathVariable  String id) {
         String[][] state = this.boardStateService.getBoardState(id);
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(state);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(state);
     }
 }
