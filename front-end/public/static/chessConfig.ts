5f53b5be104e0a4d840627157525b25609982c06
noNameRose <datnguyengithubwork@gmail.com>
2026-07-11 09:17:49 +0700

    fea: add chess dimension size

--- front-end/public/static/chessConfig.ts
+++ front-end/public/static/chessConfig.ts
@@ -1,16 +1,20 @@
 export type PieceString = "BP" | "BR" | "BQ" | "BKI" | "BK" | "BB" 
                   | "WP" | "WR" | "WQ" | "WKI" | "WK" | "WB";
 
-export const CELL_DIMENSION = 100;
+export type PlayerString = "Human" | "Claude" | "ChatGPT" | "Random";
 
-type CHATGPT_CELL_FILL = "#4FBDA1";
+export const CELL_DIMENSION = 50;
+export const PIECE_DIMENSION = CELL_DIMENSION;
+
+type CHATGPT_CELL_FILL = "#BD674C";
 type CLAUDE_CELL_FILL = "#4FBDA1";
 type RANDOM_CELL_FILL = "#4B73BD";
 
 export const CellFill = {
-    "Claude": "#4FBDA1",
+    "Claude": "#BD674C",
     "ChatGPT": "#4FBDA1",
-    "Random": "#4B73BD"
+    "Random": "#4B73BD",
+    "Human": "fasdf"
 };
 
 export type CellColor = CHATGPT_CELL_FILL | CLAUDE_CELL_FILL | RANDOM_CELL_FILL;
