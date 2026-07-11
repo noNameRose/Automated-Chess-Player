b25a0de6e0dde70e1da2f53a4b09621b2e041f4f
noNameRose <datnguyengithubwork@gmail.com>
2026-07-11 08:38:41 +0700

    fea: get player name from request params in route

--- front-end/src/pages/GamePage.tsx
+++ front-end/src/pages/GamePage.tsx
@@ -1,10 +1,13 @@
 import { useContext, useEffect } from "react";
 import TransitionContext from "../contexts/TransitionContext";
 import Board from "../chess/Board";
+import { useParams } from "react-router-dom";
+import type { PlayerString } from "../../public/static/chessConfig";
 
 const GamePage = () => {
     const transitionContext = useContext(TransitionContext);
-    
+    const {firstPlayer, secondPlayer} = useParams<{firstPlayer: PlayerString, secondPlayer: PlayerString}>();
+
     useEffect(() => {
         if (transitionContext) {
             transitionContext.handleTransition(false);
@@ -12,7 +15,10 @@ const GamePage = () => {
     }, []);
     return (
         <div>
-            <Board/>
+            <Board
+                firstPlayer={firstPlayer as PlayerString}
+                secondPlayer={secondPlayer as PlayerString}
+            />
         </div>
     );
 };
