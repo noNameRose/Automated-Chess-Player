8d7a9a1db64d2b1a6d9cd959e2c725a2e62998d8
noNameRose <datnguyengithubwork@gmail.com>
2026-07-11 12:18:13 +0700

    style: add color for each piece in chess config

--- front-end/public/static/chessConfig.ts
+++ front-end/public/static/chessConfig.ts
@@ -12,6 +12,10 @@ type CHATGPT_CELL_FILL = "#BD674C";
 type CLAUDE_CELL_FILL = "#4FBDA1";
 type RANDOM_CELL_FILL = "#4B73BD";
 
+type CHATGPT_PIECE_FILL = "#C9FFF2";
+type CLAUDE_PIECE_FILL = "#FFD6C9";
+type RANDOM_PIECE_FILL = "#C9DCFF";
+
 export const CellFill = {
     "Claude": "#BD674C",
     "ChatGPT": "#4FBDA1",
@@ -19,6 +23,14 @@ export const CellFill = {
     "Human": "fasdf"
 };
 
+export const PieceFill = {
+    "Claude": "#C9FFF2",
+    "ChatGPT": "#4FBDA1",
+    "Random": "#4B73BD",
+    "Human": "fasdf"
+};
+
+export type PieceColor = CHATGPT_PIECE_FILL | CLAUDE_PIECE_FILL | RANDOM_PIECE_FILL;
 export type CellColor = CHATGPT_CELL_FILL | CLAUDE_CELL_FILL | RANDOM_CELL_FILL;
 
 
