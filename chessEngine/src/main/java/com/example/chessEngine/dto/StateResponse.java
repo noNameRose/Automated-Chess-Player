d4b17dfed31b51fc5158cddbe3e4c0eda777fb9e
noNameRose <datnguyengithubwork@gmail.com>
2026-07-12 06:36:31 +0700

    fea: Set up state response dto

---
+++ chessEngine/src/main/java/com/example/chessEngine/dto/StateResponse.java
@@ -0,0 +1,15 @@
+package com.example.chessEngine.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class StateResponse {
+    private String[][] state;
+}
