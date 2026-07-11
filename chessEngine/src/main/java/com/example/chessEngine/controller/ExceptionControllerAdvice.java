379cc5236c1bd9130c4cfdbd3317c7828c7b6f88
noNameRose <datnguyengithubwork@gmail.com>
2026-07-12 04:55:34 +0700

    fea: handle Board state not found exception

---
+++ chessEngine/src/main/java/com/example/chessEngine/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.example.chessEngine.controller;
+
+
+import com.example.chessEngine.dto.ErrorDetails;
+import com.example.chessEngine.exception.BoardStateNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(BoardStateNotFoundException.class)
+    public ResponseEntity<ErrorDetails> boardStateNotFoundExceptionHandler() {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(
+                        ErrorDetails
+                                .builder()
+                                .message("State not found for this id")
+                                .build()
+                );
+    }
+}
