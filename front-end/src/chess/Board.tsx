c1793fa752d2c8246327dc94c97b6c61e5af6fc7
noNameRose <datnguyengithubwork@gmail.com>
2026-07-12 10:11:48 +0700

    fea: fetch board state at the beginning of the game

--- front-end/src/chess/Board.tsx
+++ front-end/src/chess/Board.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { PieceString } from "../../public/static/chessConfig";
 import { BoardEntity } from "./core/BoardEntity";
 
 export type PlayerString = "Claude" | "ChatGPT" | "Random" | "Human";          
 
-const initialState: (PieceString | null)[][]= [
+const initialState: (PieceString | null)[][] = [
     ["BR",  "BK",  "BB",  "BQ",  "BKI", "BB",  "BK",  "BR"],
     ["BP",  "BP",  "BP",  "BP",  "BP",  "BP",  "BP",  "BP"],
     [null,  null,  null,  null,  null,  null,  null,  null],
@@ -15,8 +15,12 @@ const initialState: (PieceString | null)[][]= [
     ["WR",  "WK",  "WB",  "WQ",  "WKI", "WB",  "WK",  "WR"]
 ];
 
+type StateResponse = {
+    state: (PieceString | null)[][]
+}
+
 type GameState = {
-    board: (PieceString | null)[][],
+    board: (PieceString | null)[][] | null,
     currentPlayer: PlayerString | null,
     winner: PlayerString | null,
     isGameOver: boolean,
@@ -29,24 +33,46 @@ type BoardProp = {
 }
 
 const Board = ({firstPlayer, secondPlayer}: BoardProp) => {
+    const URL = "http://localhost:8080/game"
     const [state, setState] = useState<GameState>({
-                                                    board: initialState,
+                                                    board: null,
                                                     isGameOver: false,
                                                     atStart: true,
                                                     winner: null,
                                                     currentPlayer: null
     });
 
+    let board: BoardEntity | null = null;
+    if (state.board)
+        board = BoardEntity.parse(state.board, firstPlayer, secondPlayer);
 
-    const board = BoardEntity.parse(state.board, firstPlayer, secondPlayer);
+    const fetchBoardState = async () => {
+        const response = await fetch(URL);
+        const body = (await response.json()) as StateResponse;
+        setState({
+            ...state,
+            board: body.state,
+            atStart: false
+        });
+        
+    }
 
+    useEffect(() => {
+        // Fetch board at the beginning of a new game
+        if (state.atStart) {
+            fetchBoardState()
+        }
+        else {
+
+        }
+    }, [state]);
     
     return (
         <svg viewBox="0 0 500 500" 
             className="border-2 w-screen h-screen"
         >
-            {board.renderCell()}
-            {board.renderPiece()}
+            {board && board.renderCell()}
+            {board && board.renderPiece()}
         </svg>
     );
 };
