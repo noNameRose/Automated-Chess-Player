931159cc7048a1becb8bc27a10f180f6e553b5e1
noNameRose <datnguyengithubwork@gmail.com>
2026-07-12 10:25:58 +0700

    fea: add getter and seter for move request pojp

--- chessEngine/src/main/java/com/example/chessEngine/dto/MoveRequest.java
+++ chessEngine/src/main/java/com/example/chessEngine/dto/MoveRequest.java
@@ -3,9 +3,13 @@ package com.example.chessEngine.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
 @AllArgsConstructor
+@Getter
+@Setter
 public class MoveRequest {
     private String[][] state;
     private boolean isBlack;
