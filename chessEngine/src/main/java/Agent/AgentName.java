bfa485866038d49ff3c9ede64f52804cb89b71da
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-13 17:18:17 -0500

    fix: remove 'agent' from agent name

--- chessEngine/src/main/java/Agent/AgentName.java
+++ chessEngine/src/main/java/Agent/AgentName.java
@@ -1,9 +1,9 @@
 package Agent;
 
 public final class AgentName {
-  public static final String RANDOM = "Random Agent";
-  public static final String ALPHA_BETA = "Alpha-Beta Agent";
-  public static final String GEMINI = "Gemini Agent";
-  public static final String CLAUDE = "Claude Agent";
-  public static final String HUMAN = "Human Agent";
+  public static final String RANDOM = "Random";
+  public static final String ALPHA_BETA = "Alpha-Beta";
+  public static final String GEMINI = "Gemini";
+  public static final String CLAUDE = "Claude";
+  public static final String HUMAN = "Human";
 }
