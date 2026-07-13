948a4fb812c2fcac4e38ec2d8954bf4e3d598634
noNameRose <datnguyengithubwork@gmail.com>
2026-07-13 08:13:05 +0700

    fix: remove constructor of agent not found exception

--- chessEngine/src/main/java/com/example/chessEngine/exception/AgentNotFoundException.java
+++ chessEngine/src/main/java/com/example/chessEngine/exception/AgentNotFoundException.java
@@ -1,7 +1,4 @@
 package com.example.chessEngine.exception;
 
 public class AgentNotFoundException extends RuntimeException {
-    public AgentNotFoundException(String message) {
-        super(message);
-    }
 }
