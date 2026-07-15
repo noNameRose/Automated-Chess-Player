ab99065c680933046c5ac5778f1c6b2d8bfdefca
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-15 00:23:27 -0500

    fea: set up validate move response

---
+++ chessEngine/src/main/java/com/example/chessEngine/dto/ValidateMoveResponse.java
@@ -0,0 +1,15 @@
+package com.example.chessEngine.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class ValidateMoveResponse {
+  private String[][] state;
+  private boolean isGameOver;
+}
