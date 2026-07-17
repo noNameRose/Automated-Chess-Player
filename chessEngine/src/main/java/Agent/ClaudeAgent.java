5688bf0c585095e1bc8ceddd6797170089e3ab93
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-17 15:47:04 -0500

    fea: set up claude agent

---
+++ chessEngine/src/main/java/Agent/ClaudeAgent.java
@@ -0,0 +1,15 @@
+package Agent;
+
+import ChessLogic.Board;
+
+public class ClaudeAgent extends Agent{
+
+  public ClaudeAgent(String name, boolean isMyPieceBlack) {
+      super.name = name;
+      super.myPieceIsBlack = isMyPieceBlack;
+  }
+
+  public int[] makeMove(Board board) {
+    return null;
+  }
+}
