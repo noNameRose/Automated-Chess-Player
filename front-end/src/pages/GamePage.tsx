5d4e708185d1ae3e2e6cecf6c239a5af5ba3a16f
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-21 18:36:35 -0500

    style: make board container flex

--- front-end/src/pages/GamePage.tsx
+++ front-end/src/pages/GamePage.tsx
@@ -15,7 +15,7 @@ const GamePage = () => {
         }
     }, []);
     return (
-        <div className="max-w-260 min-h-screen border-2 mx-auto overflow-hidden">
+        <div className="max-w-260 min-h-screen border-2 mx-auto overflow-hidden flex flex-col md:flex-row">
             <Board
                 firstPlayer={firstPlayer as PlayerString}
                 secondPlayer={secondPlayer as PlayerString}
