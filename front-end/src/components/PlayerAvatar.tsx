4a5cbc182a629335c9eed632c4b3c4cff6df95e2
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-21 19:18:56 -0500

    style: work on responsive design for player field in tracking panel

--- front-end/src/components/PlayerAvatar.tsx
+++ front-end/src/components/PlayerAvatar.tsx
@@ -1,13 +1,28 @@
-import type { PlayerString } from "../../public/static/chessConfig";
+import { WhiteCellFill, type PlayerString } from "../../public/static/chessConfig";
 import OptionImage from "./OptionImage";
 
 const PlayerField = ({name}: {name: PlayerString}) => {
     return (
-        <div className="flex">
-            <OptionImage
-                name={name}
-            />
-            <h1>{name}</h1>
+        <div 
+            className="flex items-center w-full p-[1em] rounded-[.5em]"
+            style={
+                {
+                    backgroundColor: WhiteCellFill[name]
+                }
+            }
+        >
+            <div
+                style={
+                    {
+                        width: "clamp(100px, 10vw, 146px)"
+                    }
+                }
+            >
+                <OptionImage
+                    name={name}
+                />
+            </div>
+            <h1 className="text-3xl font-black">{name}</h1>
         </div>
     );
 };
