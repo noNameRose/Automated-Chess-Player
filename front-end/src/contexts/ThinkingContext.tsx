a8e1e0d42302dd06616e98d5092ccf6c5649f549
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-21 20:52:02 -0500

    fea: add function in thingking contextto set state of thinking

--- front-end/src/contexts/ThinkingContext.tsx
+++ front-end/src/contexts/ThinkingContext.tsx
@@ -2,8 +2,10 @@ import { createContext } from "react";
 
 type PlayerThinking = {
     firstPlayerThinking: boolean,
-    secondPlayerThinking: boolean
-}
+    secondPlayerThinking: boolean,
+    handleFirstPlayerThinking: (isFirstPlayerThinking: boolean) => void,
+    handleSecondPlayerThinking: (isSecondPlayerThinkning: boolean) => void
+};
 
 const ThinkingContext = createContext<PlayerThinking | null>(null);
 
