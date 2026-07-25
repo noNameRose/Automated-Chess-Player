f88b0234e190cb3ceeea3c9c25f186889ab80499
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-25 16:07:13 -0500

    fea: set up dot for dot loading

--- front-end/src/components/loading/ThinkingLoading.tsx
+++ front-end/src/components/loading/ThinkingLoading.tsx
@@ -4,28 +4,38 @@ import gsap from "gsap";
 
 const ThinkingLoading = ({firstPlayer}: {firstPlayer: boolean}) => {
     const thinkingContext = useContext(ThinkingContext);
+    const dotStyle = "w-[10px] h-[10px] bg-black rounded-[50%]";
     const dom = useRef<HTMLDivElement | null>(null);
+    const dot1 = useRef<HTMLDivElement | null>(null);
+    const dot2 = useRef<HTMLDivElement | null>(null);
+    const dot3 = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        if (thinkingContext?.firstPlayerThinking === firstPlayer) {
-            gsap.to(dom.current, {
-                opacity: 1
-            });
-        }
-        else {
-            gsap.to(dom.current, {
-                opacity: 0
-            });
-        }
-    }, [thinkingContext]);
+    // useEffect(() => {
+    //     if (thinkingContext?.firstPlayerThinking === firstPlayer) {
+    //         gsap.to(dom.current, {
+    //             opacity: 1
+    //         });
+    //     }
+    //     else {
+    //         gsap.to(dom.current, {
+    //             opacity: 0
+    //         });
+    //     }
+    // }, [thinkingContext]);
+
     return (
         <div 
             ref={dom}
+            className="flex gap-4"
             style={
             {
-                opacity: 0
+                opacity: 1
             }
-        }>Thinking....</div>
+        }>
+            <div ref={dot1} className={dotStyle}></div>
+            <div ref={dot2} className={dotStyle}></div>
+            <div ref={dot3} className={dotStyle}></div>
+        </div>
     );
 };
 
