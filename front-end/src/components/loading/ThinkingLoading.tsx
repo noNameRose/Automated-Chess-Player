403195e87a3fb06ae3576c73bf748789942a0908
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-25 16:21:35 -0500

    fea: add animation for dot loading

--- front-end/src/components/loading/ThinkingLoading.tsx
+++ front-end/src/components/loading/ThinkingLoading.tsx
@@ -5,10 +5,41 @@ import gsap from "gsap";
 const ThinkingLoading = ({firstPlayer}: {firstPlayer: boolean}) => {
     const thinkingContext = useContext(ThinkingContext);
     const dotStyle = "w-[10px] h-[10px] bg-black rounded-[50%]";
+    const tl = useRef<GSAPTimeline | null>(null);
     const dom = useRef<HTMLDivElement | null>(null);
     const dot1 = useRef<HTMLDivElement | null>(null);
     const dot2 = useRef<HTMLDivElement | null>(null);
     const dot3 = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        tl.current = gsap.timeline({
+            yoyo: true,
+            repeat: -1
+        });
+        
+        tl.current.to(dot1.current, {
+            scale: 1.5,
+        })
+        .to(dot1.current, {
+            scale: 1
+        })
+        .to(dot2.current, {
+            scale: 1.5,
+        }, "<")
+        .to(dot2.current, {
+            scale: 1
+        })
+        .to(dot3.current, {
+            scale: 1.5,
+        }, "<")
+        .to(dot3.current, {
+            scale: 1
+        });
+
+        return () => {
+            tl.current = null;
+        }
+    }, []);
 
     // useEffect(() => {
     //     if (thinkingContext?.firstPlayerThinking === firstPlayer) {
