13a406cd2390b896bc2011c10f503a5c021d3c52
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-24 23:43:30 -0500

    fea: add animation for thinking... state

--- front-end/src/components/loading/ThinkingLoading.tsx
+++ front-end/src/components/loading/ThinkingLoading.tsx
@@ -1,12 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import ThinkingContext from "../../contexts/ThinkingContext";
+import gsap from "gsap";
 
 const ThinkingLoading = ({firstPlayer}: {firstPlayer: boolean}) => {
     const thinkingContext = useContext(ThinkingContext);
+    const dom = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (thinkingContext?.firstPlayerThinking === firstPlayer) {
+            gsap.to(dom.current, {
+                opacity: 1
+            });
+        }
+        else {
+            gsap.to(dom.current, {
+                opacity: 0
+            });
+        }
+    }, [thinkingContext]);
     return (
-        <div style={
+        <div 
+            ref={dom}
+            style={
             {
-                opacity: (thinkingContext?.firstPlayerThinking === firstPlayer) ? 1 : 0
+                opacity: 0
             }
         }>Thinking....</div>
     );
