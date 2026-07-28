495f77d19059761349ad4a9de215a55e729e08ae
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-07-28 17:21:42 -0500

    fea: add method to add move for player's history in context

--- front-end/src/contexts/PlayerMovesContext.tsx
+++ front-end/src/contexts/PlayerMovesContext.tsx
@@ -2,7 +2,9 @@ import { createContext } from "react";
 
 type PlayerMovesProp = {
     firstPlayerMoves: string[],
-    secondPlayerMoves: string[]
+    secondPlayerMoves: string[],
+    addFirstPlayerMoves: (move: string) => void,
+    addSecondPlayerMoves: (move: string) => void
 }
 
 const PlayerMovesContexts = createContext<PlayerMovesProp | null>(null);
