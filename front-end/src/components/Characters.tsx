8c117f63a3d5abe4ac2290c0545aceef2089f9e1
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-01 14:14:25 -0500

    style: make title responsive

--- front-end/src/components/Characters.tsx
+++ front-end/src/components/Characters.tsx
@@ -1,6 +1,12 @@
 const Characters = ({characters}: {characters: string[]}) => {
     return (
-        <div className="flex font-black text-white text-7xl">
+        <div className="flex font-black text-white"
+            style={
+                {
+                    fontSize: "clamp(37px, 5vw, 90px)"
+                }
+            }
+        >
             {characters.map(ch => (
                 <p className="character translate-y-75">
                     {ch}
