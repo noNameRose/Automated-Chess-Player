b86b2ee2f689a7ed1ba6d4bcedbd383ae4c169df
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-01 15:46:30 -0500

    style: hide characters sequence when it's out of continer

--- front-end/src/components/Title.tsx
+++ front-end/src/components/Title.tsx
@@ -68,7 +68,7 @@ const Title = () => {
                     </div>);
             })
             }
-            <div className="col-[3/14] row-[2/5] z-30 flex items-center justify-center gap-4">
+            <div className="col-[3/14] row-[2/5] z-30 flex items-center justify-center gap-4 overflow-hidden">
                 <Characters
                     characters={["A", "I"]}
                 />
