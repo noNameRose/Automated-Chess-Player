c50ce924b4ab4a35e61318ba2d0445a9edefc17b
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-01 14:06:09 -0500

    style: hide characters

--- front-end/src/components/Characters.tsx
+++ front-end/src/components/Characters.tsx
@@ -2,7 +2,7 @@ const Characters = ({characters}: {characters: string[]}) => {
     return (
         <div className="flex font-black text-white text-7xl">
             {characters.map(ch => (
-                <p className="character">
+                <p className="character translate-y-75">
                     {ch}
                 </p>
             ))}
