56d8cc8556783cb60fdef3877dcc1db66300df45
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-02 02:07:55 -0500

    fea: add slide in animation for move notation

--- front-end/src/components/MoveNotations.tsx
+++ front-end/src/components/MoveNotations.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useRef } from "react";
 import { BlackCellFill, WhiteCellFill, type PlayerString } from "../../public/static/chessConfig";
+import gsap from "gsap";
 
 const MoveNotation = ({isBlack, playerName, notation}: {isBlack: boolean, playerName: PlayerString, notation: string}) => {
+    const notationRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        gsap.to(notationRef.current, {
+            transform: "translate(0,0)"
+        });
+    }, []);
     return (
         <div
+            ref={notationRef}
             className="w-full text-center py-2 -translate-x-full"
             style={
                 {
