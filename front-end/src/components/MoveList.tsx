c504805db76e3db5934de4e759a5b61ce9b2b274
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-02 01:00:59 -0500

    style: add padding and width to move notation container

--- front-end/src/components/MoveList.tsx
+++ front-end/src/components/MoveList.tsx
@@ -2,10 +2,11 @@ import { BlackCellFill, WhiteCellFill, type PlayerString } from "../../public/st
 
 const MoveList = ({moves, playerName, isBlack}: {moves: string[], playerName: PlayerString, isBlack: boolean}) => {
     return (
-        <div className="flex flex-col gap-4 items-center grow"
+        <div className="flex flex-col gap-4 items-center grow py-3 w-1/2"
             style={
                 {
                     backgroundColor: isBlack ?  BlackCellFill[playerName] : WhiteCellFill[playerName],
+                    color: isBlack ? WhiteCellFill[playerName] : BlackCellFill[playerName]
                 }
             }
         >
