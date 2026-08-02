b447e096812db457dc092c6be4bcee7e6fe37f65
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-02 00:18:56 -0500

    style: make moves container grow as much as it can

--- front-end/src/components/MoveList.tsx
+++ front-end/src/components/MoveList.tsx
@@ -1,6 +1,6 @@
 const MoveList = ({moves}: {moves: string[]}) => {
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 items-center grow">
             {moves.map(move => (
                         <div>{move}</div>
             ))}
