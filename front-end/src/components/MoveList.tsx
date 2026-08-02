c1ba79553cfb1b55303f4b0bf30404a6a47d06c6
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-02 01:08:14 -0500

    style: move style of move notation from the container to move notation div itself

--- front-end/src/components/MoveList.tsx
+++ front-end/src/components/MoveList.tsx
@@ -2,16 +2,18 @@ import { BlackCellFill, WhiteCellFill, type PlayerString } from "../../public/st
 
 const MoveList = ({moves, playerName, isBlack}: {moves: string[], playerName: PlayerString, isBlack: boolean}) => {
     return (
-        <div className="flex flex-col gap-4 items-center grow py-3 w-1/2"
-            style={
-                {
-                    backgroundColor: isBlack ?  BlackCellFill[playerName] : WhiteCellFill[playerName],
-                    color: isBlack ? WhiteCellFill[playerName] : BlackCellFill[playerName]
-                }
-            }
+        <div className="flex flex-col items-center grow w-1/2"
         >
             {moves.map(move => (
-                        <div>{move}</div>
+                        <div
+                            className="w-full text-center py-2"
+                            style={
+                                {
+                                    backgroundColor: isBlack ?  BlackCellFill[playerName] : WhiteCellFill[playerName],
+                                    color: isBlack ? WhiteCellFill[playerName] : BlackCellFill[playerName]
+                                }
+                            }
+                        >{move}</div>
             ))}
         </div>
     );
